2f233e6fa684fc3d3823914c1f949aefb8b1087c
Ivan <vansmuhammad@gmail.com>
2020-01-29 01:44:22 +0700

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = 
+{
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": true,
+  "printWidth": 120,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": false,
+  "singleQuote": false,
+  "tabWidth": 4,
+  "trailingComma": "none",
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
+}
